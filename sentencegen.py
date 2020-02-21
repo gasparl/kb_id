c67eb3aec7e78aa5179744800617398e81bb1400
Gaspar Lukacs <37374850+gasparl@users.noreply.github.com>
2020-02-21 11:53:59 +0000

    Add files via upload

--- sentencegen.py
+++ sentencegen.py
@@ -5,10 +5,13 @@ import os
 import random
 import pyperclip
 
+basepath = "C:/research/proj_key_dynamics/masc_500k_texts/written/"
+
 #%% declare functions
 
 # data source: http://www.anc.org/data/masc/
 def get_model(path):
+    path = basepath + "/" + path
     combined_model = None
     for (dirpath, _, filenames) in os.walk(path):
         for filename in filenames:
@@ -25,22 +28,22 @@ def gen_text(markov_model, num = 10, typ = "x"):
     sentences = ""
     # length\tgenerated_text\tgen_text_lower\ttype\n"
     for _ in range(num):
-        new_s = markov_model.make_short_sentence(50, 30, tries = 50)
+        new_s = markov_model.make_short_sentence(55, 40, tries = 50)
         print(len(new_s), new_s)
         sentences += str(len(new_s)) + "\t" +  new_s + "\t" +  new_s.lower() + "\t" +  typ + "\n"
     pyperclip.copy(sentences)
 
 #%% read texts to create models
-random.seed(0)
+random.seed(1)
 
-formals = get_model("masc_500k_texts/written/formal-technical")
-informals = get_model("masc_500k_texts/written/informal-natural")
+formals = get_model("formal-technical")
+informals = get_model("informal-natural")
 
 #%% generate sentences
-random.seed(0)
+random.seed(1)
 
-gen_text(formals, 200, "formal")
+gen_text(formals, 100, "formal")
 
-gen_text(informals, 200, "informal")
+gen_text(informals, 100, "informal")
 
 
